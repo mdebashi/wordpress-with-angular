611d7ab97d274e9233d5921d0a3271e3e85961d9
Mohammed Debashi <mdebashi@gmail.com>
2016-09-28 03:23:20 +0100

    displaying correct html format through ngSanatize

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wordpress-with-angular
 Trying out the wordpress rest api with angular to create an angular wordpress theme
+
+#Installed Wordpress API
+make sure you install <code>WP REST API (WP API)<code> plugin
+This will create a JSON format of your wordpress site data including users, posts, taxonomies and more.
--- functions.php
+++ functions.php
@@ -3,17 +3,23 @@ function my_scripts() {
 	
 	wp_register_script(
 		'angularjs',
-		get_stylesheet_directory_uri() . '/scripts/node_modules/angular/angular.js'
+		get_stylesheet_directory_uri() . '/scripts/node_modules/angular/angular.min.js'
 	);
 	wp_register_script(
 		'angularjs-route',
-		get_stylesheet_directory_uri() . '/scripts/node_modules/angular-route/angular-route.js'
+		get_stylesheet_directory_uri() . '/scripts/node_modules/angular-route/angular-route.min.js'
 	);
+	wp_register_script(
+		'angularjs-sanitize',
+		get_stylesheet_directory_uri() . '/scripts/node_modules/angular-sanitize/angular-sanitize.min.js'
+	);
+	
 	wp_enqueue_script(
 		'my-scripts',
 		get_stylesheet_directory_uri() . '/scripts/scripts.js',
-		array( 'angularjs', 'angularjs-route' )
+		array( 'angularjs', 'angularjs-route', 'angularjs-sanitize' )
 	);
+
 	wp_localize_script(
 		'my-scripts',
 		'myLocalized',
