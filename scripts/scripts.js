f17918487def4cb46f44a72969618fc68fecae04
Mohammed Debashi <mdebashi@gmail.com>
2016-09-29 04:38:03 +0100

    making links use the slug property of the post instead of id

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,35 @@
 angular.module('app', ['ngRoute', 'ngSanitize'])
-.config(function($routeProvider, $locationProvider){
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 	$locationProvider.html5Mode(true);
 	$routeProvider
 	.when('/',{
 		templateUrl: myLocalized.templates + 'main.html',
 		controller: 'MainController'
 	})
-	.when('/:ID', {
+	.when('/:slug', {
 		templateUrl: myLocalized.templates + 'content.html',
 		controller: 'ContentController'
 	});
-})
-.controller('MainController', function($scope, $http, $routeParams){
+}])
+.controller('MainController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 	$http.get('wp-json/posts/').success(function(res){
 		$scope.posts = res;
 	});
-})
-.controller('ContentController', function($scope, $http, $routeParams) {
-	$http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
-		$scope.post = res;
+}])
+.controller('ContentController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$http.get('wp-json/posts/?filter[name]' + $routeParams.slug).success(function(res){
+		for (var i = res.length - 1; i >= 0; i--) {
+			if(res[i]["slug"] == $routeParams.slug){
+				$scope.post = res[i];
+			}
+		}
 	});
-})
+}])
 .directive('searchForm', function(){
 	return {
 		restrict: 'EA',
 		templateUrl: myLocalized.templates + 'search-form.html',
-		controller: function($scope, $http){
+		controller: ['$scope', '$http', function($scope, $http){
 			$scope.filter = {
 				s: ''
 			};
@@ -34,6 +38,6 @@ angular.module('app', ['ngRoute', 'ngSanitize'])
 					$scope.posts = res;
 				});
 			};
-		}
+		}]
 	};
 });
